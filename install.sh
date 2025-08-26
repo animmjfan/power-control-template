c8e03734e7f5d143e02c821b8a8454cfc7fbe515
animmjfan <animmjfan@gmail.com>
2025-08-26 10:48:47 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,34 +5,40 @@
 #########################
 
 # This script manually makes the required directories, folders and files for the script to run!
-# by default this works under /usr/local/ and uses /usr/local/bin and /usr/local/lib and under /usr/local/lib
+# by default this works under /usr/loca/ and uses /usr/local/bin and /usr/local/lib and under /usr/local/lib
 # you can modify the name and path using variables and to get the default you keep install_ver='default' if you want your changes to apply change it to install_ver='custom'
 
 program_name='Power control'
 install_ver='default'
+
 if [[ $install_ver == 'default' ]]; then
 	echo "Are you sure you want to use default values for the installation?"
-	echo "Default values will declare the installation path of the libaries to /usr/local/lib/ and the executable to /usr/local/bin/"
+	echo "Default values will declare the installation path of the libaries to /usr/local/lib and the executable to /usr/local/bin"
 	echo "all under /usr/local/"
 	echo "if you want to modify this then open this script in an editor, change the variables and make sure to change status to custom!"
-	echo -n "are you sure you want to continue (press enter for yes and ctrl-c for no): "
-	read
+	read -p "Are you sure you want to continue? [y/N]: " confirm
+	if [[ $confirm =~ ^[Yy]$ ]]; then
+		echo "Okay then!"
+	else
+		exit 1
+	fi
+	unset confirm
 	echo "Okay then!"
-	install_path='/usr/local/'
-	libary_folder='/usr/local/lib/'
-	binary_folder='/usr/local/bin/'
-	templater='animmjfan'
-	remixer=''
+	install_path='/usr/local'
+	libary_folder='/usr/local/lib'
+	binary_folder='/usr/local/bin'
+	based_on='animmjfan'
+	author=''
 elif [[ $install_ver == 'custom' ]]; then
 	# set/modify variables below
-	# co_author is you and you can change the text that shows up with the authors to credit me after you or any way you want it to say it!!
-	install_path='/usr/local/'
-	libary_folder='/usr/local/lib/'
-	binary_folder='/usr/local/bin/'
-	templater='animmjfan'
-	remixer=''
+	# co_author is you and you can change the text that shows up with the credits to credit me and you in any way you want it to say it!!
+	install_path='/usr/local'
+	libary_folder='/usr/local/lib'
+	binary_folder='/usr/local/bin'
+	based_on='animmjfan'
+	author=''
 	echo -e "install path is $(realpath $install_path)"
-	read -p "do you want to continue? [y/N]: " confirm
+	read -p "Are you sure you want to continue? [y/N]: " confirm
 	confirm=${confirm: -N}
 	if [[ $confirm =~ ^[Yy]$ ]]; then
 		echo "Okay then!"
@@ -46,9 +52,9 @@ echo -e '\n' # creates two newlines
 install_path=$(realpath $install_path)
 libary_folder=$(realpath $libary_folder)
 binary_folder=$(realpath $binary_folder)
-echo 'starting install'
-echo 'starting checks'
-echo "checking the existance of the install path at '$install_path'"
+echo "starting install"
+echo "starting checks"
+echo "checking the existance of the install path at \"$install_path\""
 if [ ! -d $install_path ]; then
 	read -p "$install_path doesn't exist. Should it be made? [y/N]: " confirm
 	confirm=${confirm: -N}
@@ -65,10 +71,11 @@ if [ ! -d $install_path ]; then
 elif [ -d $install_path ]; then
 	echo "$install_path already exists, skipping it"
 else
-	echo "This error is impossible"
+	echo "This error should be impossible"
 	echo "Be proud"
 	exit 1
 fi
+
 # continue on making the rest of the things that need to be checked for, made, and move scripts from the user setup and default files to the libary folder
 
 echo "$program_name was installed!"
